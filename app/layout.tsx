f13827fa230789e65f61acd8f54c2969c1f2175c
im-Maryam-Mahmood <159541999+im-Maryam-Mahmood@users.noreply.github.com>
2024-07-27 18:09:34 +0500

    inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
+  title: "Portfolio | Maryam",
   description: "This is my portfolio",
 };
 
